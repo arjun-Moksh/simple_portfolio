34b55ece99055d9be4a215201408207d85f666b6
arjun-Moksh <arjunkc905@gmail.com>
2022-03-13 19:55:27 +0545

    Decision and Loop

---
+++ python/Decision_Making_Loop/if_statment.py
@@ -0,0 +1,47 @@
+# x = 5
+# y = -1
+
+# if y > y:
+#     print('x is positive')
+
+# print('This is always executed')
+
+# if y > 0:
+#     print('x is positive')
+
+# print('This is always executed')
+
+# x = -1
+
+# if x >= 0:
+#     print('x is positive or zero')
+# else:
+#     print('x is negative')
+
+# print('This is always executed')
+
+# x = 0
+
+# if x >= 0:
+#     print('x is positive')
+# elif x == 0:
+#     print('x is zero')
+# else:
+#     print('x is negative')
+
+# print('This is always executed')
+
+# num = int(input('Enter a number: '))
+# print("The number you entered is : " , num)
+# result = num + 5
+# #print(type(num))
+# print(result)
+
+# num  = float(input('Enter a number: '))
+# if num >= 0:
+#     if num == 0:
+#         print('Zero')
+#     else:
+#         print('Positive number')
+# else:
+#     print('Negative number')
